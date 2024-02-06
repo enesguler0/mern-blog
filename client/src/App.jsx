c4075602f9d5b8696e2204390143c4e73b0c4a3d
enesguler0 <enesguler149@gmail.com>
2024-02-06 20:40:22 +0300

    footer comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 
-
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -22,6 +22,7 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
