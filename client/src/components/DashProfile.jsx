7539bd8bebd9619a9f1a558dd1de5ca0b399b455
enesguler0 <enesguler149@gmail.com>
2024-02-11 05:47:51 +0300

    create-post page ui(++react-quill)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -9,9 +9,10 @@ import 'react-circular-progressbar/dist/styles.css'
 
 import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice'
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
+import {Link} from 'react-router-dom'
 
 export default function DashProfile() {
-  const {currentUser, error} = useSelector(state=>state.user)
+  const {currentUser, error, loading} = useSelector(state=>state.user)
   const [imageFile, setImageFile] = useState(null)
   const [imageFileUrl, setImageFileUrl] = useState(null)
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
@@ -187,9 +188,19 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...': 'Update'}
         </Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+              <Button type='button' gradientDuoTone='purpleToPink' className='w-full' >
+                Create a post
+              </Button>
+            </Link>
+            
+          )
+        }
       </form>
       <div className='text-red-700 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
